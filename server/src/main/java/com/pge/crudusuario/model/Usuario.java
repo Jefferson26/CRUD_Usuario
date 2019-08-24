f2b281b228d3cca2ffadff208556f8c9c3273eaf
Jefferson Barbosa <jefferson.sb.es@gmail.com>
2019-08-24 09:07:48 -0300

    Adição de atributos na classe Usuario

--- server/src/main/java/com/pge/crudusuario/model/Usuario.java
+++ server/src/main/java/com/pge/crudusuario/model/Usuario.java
@@ -1,5 +1,45 @@
 package com.pge.crudusuario.model;
 
-public class Usuario {
+import java.time.LocalDate;
 
+import com.pge.crudusuario.util.GeneroUsuario;
+
+public class Usuario {
+	private Long id;
+	private String name;
+	private String cpf;
+	private LocalDate dt_nascimento;
+	private GeneroUsuario genero;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public LocalDate getDt_nascimento() {
+		return dt_nascimento;
+	}
+	public void setDt_nascimento(LocalDate dt_nascimento) {
+		this.dt_nascimento = dt_nascimento;
+	}
+	public GeneroUsuario getGenero() {
+		return genero;
+	}
+	public void setGenero(GeneroUsuario genero) {
+		this.genero = genero;
+	}
+	
 }
