ee58d6d9b7e9795a64ee36780e47e916155d2ae9
Jefferson Barbosa <jefferson.sb.es@gmail.com>
2019-08-24 20:37:07 -0300

    Adição de endpoints para criar e deletar usuario

--- server/src/main/java/com/pge/crudusuario/controller/UsuarioController.java
+++ server/src/main/java/com/pge/crudusuario/controller/UsuarioController.java
@@ -4,6 +4,10 @@ package com.pge.crudusuario.controller;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +23,20 @@ public class UsuarioController {
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
 	public List<Usuario> getUsuariosCadastrados() {
 		return usuarios;
+	}
+	
+	@DeleteMapping(path = { "/{id}" })
+	public Usuario delete(@PathVariable("id") Long id) {	
+		Usuario usuario_removido = usuarios.stream().filter(
+														c -> c.getId().equals(id)
+													).findFirst().get();		
+		return usuario_removido;
+	}
+
+	@PostMapping
+	public Usuario create(@RequestBody Usuario usuario) {
+		usuarios.add(usuario);
+		return usuario;
 	}
 	
 	private static List<Usuario> adicionaUsuariosFake() {
